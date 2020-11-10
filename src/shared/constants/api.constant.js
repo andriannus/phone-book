2ee40799fe94c931ae70fdcc49658fec9ffa1caa
Andriannus Parasian <andriannus.parasian@amarbank.co.id>
2020-11-10 16:07:47 +0700

    feat: create api-invoker service

---
+++ src/shared/constants/api.constant.js
@@ -0,0 +1,4 @@
+export const API = {
+  baseUrl: "https://randomuser.me/api",
+  timeout: 10000,
+};
---
+++ src/shared/services/api-invoker/api-invoker.service.js
@@ -0,0 +1,53 @@
+import { reactive } from "@vue";
+import axios from "axios";
+
+import { API } from "@/shared/constants/api.constant";
+
+export default function useApiInvoker({ baseUrl, headers } = {}) {
+  const state = reactive({
+    cancelSource: null,
+  });
+
+  const apiInvoker = axios.create({
+    baseURL: baseUrl || API.baseUrl,
+    timeout: API.timeout,
+    ...headers,
+  });
+
+  const createCancelToken = config => {
+    const { CancelToken } = axios;
+    const source = CancelToken.source();
+    state.cancelSource = source;
+
+    config.cancelToken = source.token;
+  };
+
+  const cancelRequest = () => {
+    if (state.cancelSource) {
+      return state.cancelSource.cancel();
+    }
+  };
+
+  const interceptRequest = config => {
+    cancelRequest();
+    createCancelToken(config);
+
+    return config;
+  };
+
+  const interceptSuccessResponse = res => {
+    return res;
+  };
+
+  const interceptErrorResponse = err => {
+    return Promise.reject(err);
+  };
+
+  apiInvoker.interceptors.request.use(interceptRequest);
+  apiInvoker.interceptors.response.use(
+    interceptSuccessResponse,
+    interceptErrorResponse,
+  );
+
+  return { apiInvoker };
+}
