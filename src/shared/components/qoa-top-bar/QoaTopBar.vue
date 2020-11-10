a5c7826f4bc6f32f7d8f33d22178fe683a0c3456
Andriannus Parasian <andriannus.parasian@amarbank.co.id>
2020-11-10 16:30:17 +0700

    feat: create top-bar component

---
+++ src/shared/components/qoa-top-bar/QoaTopBar.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="TopBar">
+    <div class="TopBar-logo">
+      <span class="FontSize-large">
+        <strong>Qoala Test</strong>
+      </span>
+    </div>
+
+    <div class="TopBar-actions">
+      <div class="TopBar-action">
+        <button class="Button Button--primary" type="button">
+          Color
+        </button>
+      </div>
+
+      <div class="TopBar-action">
+        <button class="Button Button--primary Button--outlined" type="button">
+          Cities
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "QoaTopBar",
+};
+</script>
