1db5f4680a29900feb9378e1b8675de11cf81923
Andriannus Parasian <andriannus.parasian@amarbank.co.id>
2020-11-10 16:30:30 +0700

    refactor: implement top-bar component

--- src/app/landing/Landing.vue
+++ src/app/landing/Landing.vue
@@ -1,9 +1,15 @@
 <template>
-  <h1>Hello World</h1>
+  <qoa-top-bar></qoa-top-bar>
 </template>
 
 <script>
+import QoaTopBar from "@/shared/components/qoa-top-bar/QoaTopBar";
+
 export default {
   name: "Landing",
+
+  components: {
+    QoaTopBar,
+  },
 };
 </script>
