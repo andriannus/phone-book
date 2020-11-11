4dab57b1d345c2cc97cfb670cc3def9baf9ef19f
Andriannus Parasian <andriannus.parasian@amarbank.co.id>
2020-11-11 22:40:36 +0700

    feat: create component for mobile and desktop view

---
+++ src/app/landing/shared/components/landing-desktop/LandingDesktop.vue
@@ -0,0 +1,146 @@
+<template>
+  <div class="Landing Container" @scroll="onHorizontalScroll">
+    <div
+      v-for="(user, index) in props.paginatedUsers.data"
+      :key="index"
+      class="Landing-column"
+    >
+      <qoa-card :className="cardClassName(user.color)">
+        <div class="Landing-image TextAlign-center">
+          <img :src="user.picture.thumbnail" alt="Picture" />
+        </div>
+
+        <p>
+          <strong>{{ fullName(user.name) }}</strong>
+        </p>
+        <p>{{ user.dob.age }} years old</p>
+        <p>{{ address(user.location) }}</p>
+        <p>{{ user.email }}</p>
+      </qoa-card>
+    </div>
+  </div>
+</template>
+
+<script>
+import { onMounted } from "vue";
+
+import QoaCard from "@/shared/components/qoa-card/QoaCard";
+import { transformAddress, transformFullName } from "@/shared/utils/transform";
+
+export default {
+  name: "LandingDesktop",
+
+  components: {
+    QoaCard,
+  },
+
+  props: {
+    paginatedUsers: {
+      type: Object,
+      default() {
+        return {};
+      },
+    },
+  },
+
+  setup(props, { emit }) {
+    onMounted(() => {
+      //
+    });
+
+    const isStillScrollable = () => {
+      const container = document.querySelector(".Landing");
+      const rightOfPage = container.scrollWidth - container.clientWidth - 100;
+
+      return container.scrollLeft >= rightOfPage;
+    };
+
+    const onHorizontalScroll = () => {
+      if (!props.paginatedUsers.meta.nextPage) return;
+
+      if (isStillScrollable()) {
+        emit("updated", props.paginatedUsers.meta.nextPage);
+      }
+    };
+
+    const fullName = name => {
+      return transformFullName(name);
+    };
+
+    const address = location => {
+      return transformAddress(location);
+    };
+
+    const cardClassName = color => {
+      const className = "Landing-card";
+
+      switch (color) {
+        case "red":
+          return `${className} BgColor-danger`;
+        case "green":
+          return `${className} BgColor-primary`;
+        case "blue":
+          return `${className} BgColor-secondary`;
+        default:
+          return;
+      }
+    };
+
+    return { address, cardClassName, fullName, onHorizontalScroll, props };
+  },
+};
+</script>
+<style lang="scss" scoped>
+.Landing {
+  display: flex;
+  flex-direction: row;
+  margin-bottom: 0;
+  margin-top: 1rem;
+  min-height: calc(100vh - 56px - 1rem);
+  overflow-x: auto;
+  padding-bottom: 1rem;
+
+  &-image {
+    img {
+      border-radius: 50%;
+    }
+  }
+
+  &-column {
+    background-color: whitesmoke;
+    border-radius: 0.5rem;
+    padding: 1rem;
+
+    &:not(:last-child) {
+      margin-right: 1rem;
+    }
+  }
+
+  &-card {
+    height: auto;
+    min-width: 350px;
+
+    &:not(:last-child) {
+      margin-right: 1rem;
+    }
+
+    *:not(:last-child) {
+      margin-bottom: 1rem;
+    }
+  }
+}
+
+::-webkit-scrollbar {
+  background-color: #f5f5f5;
+
+  &-thumb {
+    background-color: #000000;
+    border: 2px solid #555555;
+  }
+
+  &-track {
+    background-color: #f5f5f5;
+    box-shadow: inset 0 0 6px rgba(0, 0, 0, 0.3);
+  }
+}
+</style>
---
+++ src/app/landing/shared/components/landing-mobile/LandingMobile.vue
@@ -0,0 +1,127 @@
+<template>
+  <div class="Landing Container">
+    <qoa-card
+      v-for="(user, index) in paginatedUsers.data"
+      :key="index"
+      :className="cardClassName(user.color)"
+    >
+      <div class="Flex MarginBottom-base">
+        <div class="Landing-image MarginRight-base">
+          <img :src="user.picture.thumbnail" alt="Picture" />
+        </div>
+
+        <div>
+          <p class="MarginBottom-xsmall">{{ fullName(user.name) }}</p>
+          <p class="MarginBottom-xsmall">{{ user.dob.age }} years old</p>
+          <p>{{ user.email }}</p>
+        </div>
+      </div>
+
+      <p>{{ address(user.location) }}</p>
+    </qoa-card>
+  </div>
+</template>
+
+<script>
+import { onMounted, onUnmounted } from "vue";
+
+import QoaCard from "@/shared/components/qoa-card/QoaCard";
+import { transformAddress, transformFullName } from "@/shared/utils/transform";
+
+export default {
+  name: "LandingMobile",
+
+  components: {
+    QoaCard,
+  },
+
+  props: {
+    paginatedUsers: {
+      type: Object,
+      default() {
+        return {};
+      },
+    },
+  },
+
+  setup(props, { emit }) {
+    onMounted(() => {
+      window.addEventListener("scroll", onVerticalScroll);
+    });
+
+    onUnmounted(() => {
+      window.removeEventListener("scroll", onVerticalScroll);
+    });
+
+    const isStillScrollable = () => {
+      const container = document.querySelector(".Landing");
+      const { body, documentElement } = document;
+      const bottomOfPage = container.scrollHeight - window.screen.height - 100;
+
+      return (
+        documentElement.scrollTop >= bottomOfPage ||
+        body.scrollTop >= bottomOfPage
+      );
+    };
+
+    const onVerticalScroll = () => {
+      if (!props.paginatedUsers.meta.nextPage) return;
+      if (!isStillScrollable()) return;
+
+      emit("updated", props.paginatedUsers.meta.nextPage);
+    };
+
+    const fullName = name => {
+      return transformFullName(name);
+    };
+
+    const address = location => {
+      return transformAddress(location);
+    };
+
+    const cardClassName = color => {
+      const className = "Landing-card";
+
+      switch (color) {
+        case "red":
+          return `${className} BgColor-danger`;
+        case "green":
+          return `${className} BgColor-primary`;
+        case "blue":
+          return `${className} BgColor-secondary`;
+        default:
+          return;
+      }
+    };
+
+    return { address, cardClassName, fullName, props };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+@import "@amar-ui-web/responsive/scss/mixins";
+
+.Landing {
+  margin-top: 1rem;
+  margin-bottom: 1rem;
+  display: flex;
+  flex-direction: column;
+
+  &-card {
+    p {
+      line-height: 1rem;
+    }
+
+    &:not(:last-child) {
+      margin-bottom: 1rem;
+    }
+  }
+
+  &-image {
+    img {
+      border-radius: 50%;
+    }
+  }
+}
+</style>
