91a7695a75411a26a63ba260da763ad4abbe2cf2
Andriannus Parasian <andriannus.parasian@amarbank.co.id>
2020-11-12 13:08:11 +0700

    feat: create slide-up transition

---
+++ src/shared/transitions/QoaSlideUpTransition.vue
@@ -0,0 +1,26 @@
+<template>
+  <transition name="slide-up">
+    <slot></slot>
+  </transition>
+</template>
+
+<script>
+export default {
+  name: "MySlideUpTransition",
+};
+</script>
+
+<style lang="scss" scoped>
+.slide-up {
+  &-enter-active,
+  &-leave-active {
+    transition: all 0.3s ease-in-out;
+  }
+
+  &-enter-from,
+  &-leave-to {
+    transform: translateY(100px);
+    opacity: 0;
+  }
+}
+</style>
