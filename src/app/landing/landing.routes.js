98cbb04fab72ec744e0f4977aa67b31a1ffc9aca
Andriannus Parasian <andriannus.parasian@amarbank.co.id>
2020-11-12 10:38:28 +0700

    refactor: use landing for main path

--- src/app/landing/landing.routes.js
+++ src/app/landing/landing.routes.js
@@ -1,6 +1,10 @@
 const landingRoutes = [
   {
     path: "/",
+    redirect: "/landing",
+  },
+  {
+    path: "/landing",
     name: "Landing",
     component: () => import(/* webpackChunkName: "landing" */ "./Landing.vue"),
   },
