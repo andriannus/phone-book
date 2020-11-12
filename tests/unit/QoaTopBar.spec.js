f861cba85852554358a186bd631d9d3a2f8dd4b0
Andriannus Parasian <andriannus.parasian@amarbank.co.id>
2020-11-12 09:57:37 +0700

    test: create unit test for qoa-top-bar component

---
+++ tests/unit/QoaTopBar.spec.js
@@ -0,0 +1,46 @@
+import { shallowMount } from "@vue/test-utils";
+import QoaTopBar from "@/shared/components/qoa-top-bar/QoaTopBar.vue";
+
+describe("QoaTopBar.vue", () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = shallowMount(QoaTopBar);
+  });
+
+  it("should created", () => {
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it("should emit when color button clicked", () => {
+    const sortByColorButton = wrapper.find("#BtnSortByColor");
+
+    sortByColorButton.trigger("click");
+
+    expect(wrapper.emitted("sorted")).toBeTruthy();
+  });
+
+  it("should emit with `color` argument when color button clicked", () => {
+    const sortByColorButton = wrapper.find("#BtnSortByColor");
+
+    sortByColorButton.trigger("click");
+
+    expect(wrapper.emitted("sorted")[0]).toEqual(["color"]);
+  });
+
+  it("should emit when cities button clicked", () => {
+    const sortByCitiesButton = wrapper.find("#BtnSortByCities");
+
+    sortByCitiesButton.trigger("click");
+
+    expect(wrapper.emitted("sorted")).toBeTruthy();
+  });
+
+  it("should emit with `cities` argument when color button clicked", () => {
+    const sortByCitiesButton = wrapper.find("#BtnSortByCities");
+
+    sortByCitiesButton.trigger("click");
+
+    expect(wrapper.emitted("sorted")[0]).toEqual(["cities"]);
+  });
+});
