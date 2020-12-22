fa23aac4359954b2b81ffcd4332bf9c8ab9ecc4b
Andre Simamora <andriannus.parasian@amarbank.co.id>
2020-12-22 19:38:22 +0700

    chore: build to modern browser

--- vue.config.js
+++ vue.config.js
@@ -8,14 +8,10 @@ module.exports = {
       },
     },
   },
+
   chainWebpack: config => {
     config.plugin("html").tap(options => {
       options[0].title = "Phone Book";
-      return options;
-    });
-
-    config.plugin("preload").tap(options => {
-      options[0].include = "allChunks";
       return options;
     });
   },
