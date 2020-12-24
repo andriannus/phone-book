235067d43a5ea98fd264737d1d378016c97e101b
Andre Simamora <andriannus.parasian@amarbank.co.id>
2020-12-24 22:27:12 +0700

    chore: preload landing css

--- vue.config.js
+++ vue.config.js
@@ -9,10 +9,17 @@ module.exports = {
     },
   },
 
-  chainWebpack: config => {
+  chainWebpack(config) {
     config.plugin("html").tap(options => {
       options[0].title = "Phone Book";
       return options;
     });
+
+    if (config.plugins.has("preload")) {
+      config.plugin("preload").tap(options => {
+        options[0].include = ["app", "landing"];
+        return options;
+      });
+    }
   },
 };
