222d6e7dbb4d05c2fc291213fa5f51c02e7e2715
Andre Simamora <andriannus.parasian@amarbank.co.id>
2020-12-25 11:51:13 +0700

    chore: remove wrong preload syntax

--- vue.config.js
+++ vue.config.js
@@ -14,12 +14,5 @@ module.exports = {
       options[0].title = "Phone Book";
       return options;
     });
-
-    if (config.plugins.has("preload")) {
-      config.plugin("preload").tap(options => {
-        options[0].include = ["app", "landing"];
-        return options;
-      });
-    }
   },
 };
