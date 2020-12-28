fe40b41a518db74d518b0144301fbbff7478534c
Andre Simamora <andriannus.parasian@amarbank.co.id>
2020-12-28 16:45:56 +0700

    refactor: remove unused constant

--- src/app/landing/shared/constants/landing.constant.ts
+++ src/app/landing/shared/constants/landing.constant.ts
@@ -1,14 +1,2 @@
-export const CARD_CLASS = {
-  blue: "BgColor-secondary",
-  green: "BgColor-primary",
-  red: "BgColor-danger",
-};
-
 export const QUERY_PARAMS =
   "?results=100&inc=name,dob,registered,email,location,picture";
-
-export const USER_COLOR = {
-  blue: "blue",
-  green: "green",
-  red: "red",
-};
