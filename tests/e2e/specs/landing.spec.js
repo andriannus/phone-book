468426cb6ce578545d3ad6e3a860f81476e2ca51
Andre Simamora <andriannus.parasian@amarbank.co.id>
2021-01-01 16:48:41 +0700

    test: create e2e test for landing page

---
+++ tests/e2e/specs/landing.spec.js
@@ -0,0 +1,107 @@
+describe("Landing Page", () => {
+  describe("Desktop", () => {
+    beforeEach(() => {
+      cy.viewport("macbook-13");
+    });
+
+    it("should show contact when fetch is success", () => {
+      cy.intercept("api/*", {
+        fixture: "random-user",
+      }).as("fetchUsers");
+
+      cy.visit("/");
+      cy.wait("@fetchUsers");
+
+      cy.get(".LandingDesktop-card");
+    });
+
+    it("should show 10 user if the page query is `1`", () => {
+      cy.intercept("api/*", {
+        fixture: "random-user",
+      }).as("fetchUsers");
+
+      cy.visit("/");
+      cy.wait("@fetchUsers");
+
+      cy.get(".LandingDesktop")
+        .find(".LandingDesktop-card")
+        .should("have.length", 10);
+    });
+
+    it("should show 20 user if the page query is `2`", () => {
+      cy.intercept("api/*", {
+        fixture: "random-user",
+      }).as("fetchUsers");
+
+      cy.visit("/landing?page=2");
+      cy.wait("@fetchUsers");
+
+      cy.get(".LandingDesktop")
+        .find(".LandingDesktop-card")
+        .should("have.length", 20);
+    });
+
+    it("should show error content when fetch user is failed", () => {
+      cy.intercept("api/*", {
+        statusCode: 500,
+      });
+
+      cy.visit("/");
+
+      cy.contains("p", "Something wrong.");
+    });
+  });
+
+  describe("Mobile", () => {
+    beforeEach(() => {
+      cy.viewport("iphone-6");
+    });
+
+    it("should show contact when fetch is success", () => {
+      cy.intercept("api/*", {
+        fixture: "random-user",
+      }).as("fetchUsers");
+
+      cy.visit("/");
+      cy.wait("@fetchUsers");
+
+      cy.get(".LandingMobile-card");
+    });
+
+    it("should show 10 user if the page query is `1`", () => {
+      cy.intercept("api/*", {
+        fixture: "random-user",
+      }).as("fetchUsers");
+
+      cy.visit("/");
+      cy.wait("@fetchUsers");
+
+      cy.get(".LandingMobile")
+        .find(".LandingMobile-card")
+        .should("have.length", 10);
+    });
+
+    it("should show 20 user if the page query is `2`", () => {
+      cy.intercept("api/*", {
+        fixture: "random-user",
+      }).as("fetchUsers");
+
+      cy.visit("/landing?page=2");
+      cy.wait("@fetchUsers");
+
+      cy.get(".LandingMobile")
+        .find(".LandingMobile-card")
+        .should("have.length", 20);
+    });
+
+    it("should show error content when fetch user is failed", () => {
+      cy.intercept("api/*", {
+        statusCode: 500,
+      });
+
+      cy.visit("/");
+
+      cy.contains("p", "Something wrong.");
+    });
+  });
+});
