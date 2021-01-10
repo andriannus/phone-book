cbe22a2688ee4a5d0f431df6a59b8683e5767dcd
Andriannus Parasian <andriannus.parasian@qoala.id>
2021-01-10 20:07:54 +0700

    chore: update commitlint rules

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,3 +1,25 @@
 module.exports = {
   extends: ["@commitlint/config-conventional"],
+  rules: {
+    "type-enum": () => {
+      return [
+        2,
+        "always",
+        [
+          "chore",
+          "revert",
+          "feat",
+          "fix",
+          "improvement",
+          "docs",
+          "style",
+          "refactor",
+          "perf",
+          "test",
+          "build",
+          "ci",
+        ],
+      ];
+    },
+  },
 };
