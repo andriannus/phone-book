9dba1d1ce16057f80aae4f2659eb348be360edf9
Andriannus Parasian <andriannus.parasian@qoala.id>
2021-01-15 13:53:59 +0700

    feat: setup pwa

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import { createApp } from "vue";
 import App from "@/app/app.component.vue";
 import router from "@/app/app.routes";
 
+import "./register-service-worker";
+
 createApp(App)
   .use(router)
   .mount("#app");
